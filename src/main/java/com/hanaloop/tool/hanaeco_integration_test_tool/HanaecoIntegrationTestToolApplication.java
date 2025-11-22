fef4df1c7f68508d60633731a63e0eebee77a583
Young Suk Ahn Park <ys.ahnpark@hanaloop.com>
2025-11-22 15:03:50 +0900

    first commit

---
+++ src/main/java/com/hanaloop/tool/hanaeco_integration_test_tool/HanaecoIntegrationTestToolApplication.java
@@ -0,0 +1,13 @@
+package com.hanaloop.tool.hanaeco_integration_test_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaecoIntegrationTestToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanaecoIntegrationTestToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanaloop/tool/hanaeco_integration_test_tool/HanaecoIntegrationTestToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanaloop.tool.hanaeco_integration_test_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaecoIntegrationTestToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
